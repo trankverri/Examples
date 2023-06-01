f7ae3a0c72a9b3f3661eaf7d8d6c3e00218f0cc6
trankverri <trankverri@vk.com>
2023-06-01 09:04:48 +0300

    Example005

--- ex005_hello/Program.cs
+++ ex005_hello/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Console.Beep();
 Console.WriteLine("Hello, Masha!");
+Console.WriteLine("Нажмите любую клавишу, а затем Enter");
+int key1 = Console.Read();
+Console.WriteLine($"Код нажатой клавиши: {key1}");
+Console.WriteLine("Символьное представление: " + Convert.ToChar(key1));
 
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+Console.WriteLine("Нажмите любую клавишу:");
+var key2 = Console.ReadKey();
+Console.WriteLine(key2.Key);
+Console.WriteLine(key2.KeyChar);
+
