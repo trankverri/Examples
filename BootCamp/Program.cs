880148fe048f45a200dc38c5523df8f0a471a8ed
trankverri <trankverri@vk.com>
2023-06-28 09:15:15 +0300

    add BootCamp

---
+++ BootCamp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Input numb elements");
+int n=int.Parse(Console.ReadLine()!);
+int[] array=new int[n];
+for (int i = 0; i < array.Length; i++){
+    array[i]-new Random().Next(-20,20);}
+Console.WriteLine($"Начальный массии: [{string.join(", ",array)}]");
